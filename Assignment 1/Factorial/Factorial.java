966ff0b827344e2724b3eea7b9eb09b47b7d2aa0
rilpoole <rilpoole@gmail.com>
2024-06-11 09:50:32 -0700

    RP 06/11/24 09:50 AM

--- Assignment 1/Factorial/Factorial.java
+++ Assignment 1/Factorial/Factorial.java
@@ -1,4 +1,4 @@
-public class Assignment1Problem4{  
+public class Factorial{  
     /**
      * Returns the factorial of an integer input value.
      * Uses the recursive implementation.  
@@ -7,13 +7,13 @@ public class Assignment1Problem4{
      * @return The factorial. 
      * @throws RuntimeException If return exceeds maximum integer value. Returns overflow error.
      */
-    public static int Factorial(int n){
+    public static int calculate(int n){
         if(n < 0){ //negative values can cause stack overflow
             throw new RuntimeException("Possible negative input value.");
         }
         if(n == 0) return 1; //base case
         else{ //recursive case
-            int ret = n * Factorial(--n);
+            int ret = n * calculate(--n);
             if(n >= 32){
                 throw new RuntimeException("Possible overflow error. Return may exceed maximum integer value.");
             }
